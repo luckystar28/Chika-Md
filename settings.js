0df2c3d1260f447ac17f72fe84a42b79f6379ac0
Sam <78916281+luckystar28@users.noreply.github.com>
2022-12-11 03:21:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6287866148038','6287866148038','6287866148038'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6287866148038'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6287866148038' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Sam' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Luckystar-' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Sam' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
